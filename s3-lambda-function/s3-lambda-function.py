a97a8723dc577faf7fe26fc21664b23eacf69184
mani <manishru99@gmail.com>
2025-05-19 21:00:24 -0500

    Adding proj files

---
+++ s3-lambda-function/s3-lambda-function.py
@@ -0,0 +1,37 @@
+# This Lambda function is triggered by an S3 event when a file is uploaded to a specific S3 bucket.
+# It performs operations such as sending a notification via SNS or triggering another Lambda function.
+
+import boto3
+import json
+
+def lambda_handler(event, context):
+    # Extract relevant information from the S3 event trigger
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+
+    # Perform desired operations with the uploaded file
+    print(f"File '{object_key}' was uploaded to bucket '{bucket_name}'")
+
+    # Example: Send a notification via SNS
+    sns_client = boto3.client('sns')
+    # <account-id> to be replaced with my AWS acc ID
+    topic_arn = 'arn:aws:sns:us-east-1:<account-id>:s3-lambda-sns'
+    sns_client.publish(
+       TopicArn=topic_arn,
+       Subject='S3 Object Created',
+       Message=f"File '{object_key}' was uploaded to bucket '{bucket_name}'"
+    )
+
+    # Example: Trigger another Lambda function
+    # lambda_client = boto3.client('lambda')
+    # target_function_name = 'my-another-lambda-function'
+    # lambda_client.invoke(
+    #    FunctionName=target_function_name,
+    #    InvocationType='Event',
+    #    Payload=json.dumps({'bucket_name': bucket_name, 'object_key': object_key})
+    # )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Lambda function executed successfully')
+    }
